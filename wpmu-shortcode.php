3cca07c73f601fc068776594b3b3089dd7dec296
Damilare <oluwadamilareshobowale@gmail.com>
2023-04-12 11:13:21 +0100

    set condition to check if the user_firstname is not empty

--- wpmu-shortcode.php
+++ wpmu-shortcode.php
@@ -12,11 +12,24 @@ License: GPL2
 
 function subform() {
     
-    // Get the current user's display name
-    $user_display_name = wp_get_current_user()->user_firstname;
+    $user_display_name = __('Guest', 'wpmu-shortcode');
+    
+    // Check if the user is logged in
+    if(is_user_logged_in()) {
+      
+      // check if the user_firstname is not empty and then set the current user's display name
+      if(!empty(wp_get_current_user()->user_firstname)) {
+          $user_display_name = wp_get_current_user()->user_firstname;
+      } else {
+          // if the user_firstname is empty, use User to indicate logged in user
+          $user_display_name = __('User', 'wpmu-shortcode');
+      }
+    }
     
     // Get the blog name
     $blog_name = get_bloginfo( 'name' );
+    
+    ob_start();
     
     // Display the welcome message and subscription form
     echo '<p>Hey ' . $user_display_name . ', welcome to ' . $blog_name . '! You can subscribe to our newsletter here:</p>';
@@ -27,5 +40,7 @@ function subform() {
         <input type="submit" value="Submit">
     </form>
     <?php
+    
+    return ob_get_clean();
 }
 add_shortcode( 'subscriptionform', 'subform' );
